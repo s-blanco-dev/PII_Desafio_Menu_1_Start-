80aa3f0852dd156ef829341860b22c2a3bc7d37d
eugeniaguibernau <eugenia.guibernau@correo.ucu.edu.uy>
2024-08-21 09:04:01 -0300

    Clase dish

--- src/Dish.cs
+++ src/Dish.cs
@@ -5,5 +5,40 @@ namespace Ucu.Poo.Restaurant;
 /// </summary>
 public class Dish
 {
-    
+    private string name;
+
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+        
+    }
+
+    private double price;
+
+    public double Price
+    {
+        get
+        {
+            return this.price;
+        }
+    }
+
+    private bool isVegetarian;
+    public bool IsVegetarian
+    {
+        get
+        {
+            return this.isVegetarian;
+        }
+    }
+
+    public Dish(string name, double price, bool isVegetarian)
+    {
+        this.name = name;
+        this.price = price;
+        this.isVegetarian = isVegetarian;
+    }
 }
